74a2d64823451c38a9b3ad0a0c40e9a01d3cdcc2
ureishi <victorique2020+github@gmail.com>
2022-03-03 05:15:09 +0900

    Limit the test cases
    Limit the test cases to those that escape braces, to make the purpose of the test clearer.
    Rename the class to a more restrictive name.

---
+++ Assets/UdonSharp/Tests~/TestScripts/BugTests/BracesInStringInterpolation/BracesInStringInterpolation.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonSharp.Tests
+{
+    [AddComponentMenu("Udon Sharp/Tests/BracesInStringInterpolation")]
+    public class BracesInStringInterpolation : UdonSharpBehaviour
+    {
+        [System.NonSerialized]
+        public IntegrationTestSuite tester;
+
+        public void ExecuteTests()
+        {
+            Method();
+        }
+
+        public void Method()
+        {
+            tester.TestAssertion("String interpolation braces escape 1", $"}}, {{}}, {{" == "}, {}, {");
+
+            tester.TestAssertion("String interpolation braces escape 2", $"{{{1}}}, {{{{{2}}}}}, {{{{{{{3}}}}}}}" == "{1}, {{2}}, {{{3}}}");
+        }
+    }
+}
